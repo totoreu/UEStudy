9a5c8eb7772401c68ff1f8e674fdf6ba0f2eed1f
God_win <96413770+totoreu@users.noreply.github.com>
2023-10-25 04:44:47 +0900

    Add files via upload

---
+++ Composition/LessonInterface.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "LessonInterface.generated.h"
+
+
+// 딱히 뭘할게 없는 타입정보를 보관할 클래스
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class ULessonInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class UNREALCOMPOSITION_API ILessonInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	virtual void DoLesson()
+	{
+		UE_LOG(LogTemp, Log, TEXT("수업에 입장 합니다."));
+	}
+};
---
+++ Composition/MyGameInstance.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyGameInstance.h"
+#include "Student.h"
+#include "Teacher.h"
+#include "Staff.h"
+#include "Card.h"
+
+UMyGameInstance::UMyGameInstance()
+{
+    // CDO의 기본값을 설정하는 코드
+    SchoolName = TEXT("기본학교");
+}
+
+void UMyGameInstance::Init()
+{
+    Super::Init();
+
+    UE_LOG(LogTemp, Log, TEXT("==========================="));
+    TArray<UPerson*> Persons = {NewObject<UStudent>(), NewObject<UTeacher>(), NewObject<UStaff>()};
+    for (const auto Person : Persons)
+    {
+        const UCard* OwnCard = Person->GetCard();
+        check(OwnCard);
+        //if (OwnCard) { } 어차피 카드가 다있는데, 이런 경우엔 if문 대신 체크를 해주자
+
+        ECardType CardType = OwnCard->GetCardType();
+        UE_LOG(LogTemp, Log, TEXT("%s님이 소유한 카드는 %d입니다."), *Person->GetName(), CardType);
+
+        const UEnum* CardEnumType = FindObject<UEnum>(nullptr, TEXT("/Script/UnrealComposition.ECardType"));
+        if (CardEnumType)
+        {
+            FString CardMetaData = CardEnumType->GetDisplayNameTextByValue((int64)CardType).ToString();
+            UE_LOG(LogTemp, Log, TEXT("%s님이 소유한 카드명은 %s입니다."), *Person->GetName(), *CardMetaData);
+        }
+    }
+
+    UE_LOG(LogTemp, Log, TEXT("==========================="));
+}
