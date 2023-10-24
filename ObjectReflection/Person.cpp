671c549d523f8a7e1f31cbc879282c8ad30934a8
God_win <96413770+totoreu@users.noreply.github.com>
2023-10-24 21:33:41 +0900

    Create Person.cpp

---
+++ ObjectReflection/Person.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Person.h"
+
+UPerson::UPerson()
+{
+    Name = TEXT("홍길동");
+    Year = 1;
+}
+
+void UPerson::DoLesson()
+{
+    UE_LOG(LogTemp, Log, TEXT("%s님이 수업에 참여합니다."), *Name);
+}
+
+const FString& UPerson::GetName() const
+{
+    // TODO: insert return statement here
+    return Name;
+}
+
+void UPerson::SetName(const FString& InName)
+{
+    Name = InName;
+}
