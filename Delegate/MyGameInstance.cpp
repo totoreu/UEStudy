4876606a7d107825e1d1efeb996abc9f9e77b364
God_win <96413770+totoreu@users.noreply.github.com>
2023-10-26 21:05:27 +0900

    Add files via upload

---
+++ Delegate/MyGameInstance.cpp
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyGameInstance.h"
+#include "Student.h"
+#include "Teacher.h"
+#include "Staff.h"
+#include "Card.h"
+#include "CourseInfo.h"
+
+UMyGameInstance::UMyGameInstance()
+{
+    // CDO의 기본값을 설정하는 코드
+    SchoolName = TEXT("학교");
+}
+
+void UMyGameInstance::Init()
+{
+    Super::Init();
+
+    CourseInfo = NewObject<UCourseInfo>(this);
+
+    UStudent* Student1 = NewObject<UStudent>();
+    Student1->SetName(TEXT("학생1"));
+    UStudent* Student2 = NewObject<UStudent>();
+    Student2->SetName(TEXT("학생2"));
+    UStudent* Student3 = NewObject<UStudent>();
+    Student3->SetName(TEXT("학생3"));
+
+    CourseInfo->OnChanged.AddUObject(Student1, &UStudent::GetNotification);
+    CourseInfo->OnChanged.AddUObject(Student2, &UStudent::GetNotification);
+    CourseInfo->OnChanged.AddUObject(Student3, &UStudent::GetNotification);
+
+    CourseInfo->ChangeCourseInfo(SchoolName, TEXT("변경된 학사 정보"));
+
+
+    UE_LOG(LogTemp, Log, TEXT("==========================="));
+}
---
+++ Delegate/Person.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "Person.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALDELEGATE_API UPerson : public UObject
+{
+	GENERATED_BODY()
+	
+public:
+	UPerson();
+
+	// 이런 간단한 함수들은 인라인을 시도하는게 좋다.
+	// 100% 인라인으로 동작한다는 보장은 없지만 그래도 시도하는게 좋다.
+	FORCEINLINE const FString& GetName() const { return Name; }
+	FORCEINLINE void SetName(const FString& InName) { Name = InName; }
+
+	FORCEINLINE class UCard* GetCard() const { return Card; }
+	FORCEINLINE void SetCard(class UCard* InCard) { Card = InCard; }
+
+protected:
+	UPROPERTY()
+	FString Name;
+
+	//컴포지션 관계에선 전방선언 통해 include 하지 않고, 포인터로 관리하며, 의존성을 최대한 없앨수 있음
+	UPROPERTY()
+	TObjectPtr<class UCard> Card;	// (UE5 기준)
+
+	//class UCard* Card; (UE4 기준)
+
+
+private:
+	
+};
